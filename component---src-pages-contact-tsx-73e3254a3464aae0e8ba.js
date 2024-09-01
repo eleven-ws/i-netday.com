ccc8b8ed9c76b8ddf630cd16653699420fd05c8f
Kojiro Ozaki <ozaki@eleven-ws.com>
2024-09-01 19:03:23 +0900

    Updates

---
+++ component---src-pages-contact-tsx-73e3254a3464aae0e8ba.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunki_netday_com=self.webpackChunki_netday_com||[]).push([[313],{2511:function(n,u,e){e.r(u),e.d(u,{Head:function(){return Y}});var l=e(4041),t=e(5257),c=e(3452),s=e(9421),a=e(1042),f=e(7212),r=e(8291),o=e(7437);u.default=()=>{const[n,u]=l.useState(!1);return l.useEffect((()=>{u(!0)}),[]),(0,o.Y)(s.PE,null,(0,o.Y)(c.A,null,(0,o.Y)(t.A,null,(0,o.Y)(r.B,null),(0,o.Y)(f.w,null))))};const Y=()=>(0,o.Y)(a.G,null)}}]);
+//# sourceMappingURL=component---src-pages-contact-tsx-73e3254a3464aae0e8ba.js.map
